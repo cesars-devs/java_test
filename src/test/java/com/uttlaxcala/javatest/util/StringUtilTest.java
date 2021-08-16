1bde5fc4f8d71524b390c853bed76735bfe76670
Cesar Lima <cjli.x3@gmail.com>
2021-08-16 00:02:51 -0500

    2.2 Lanzar excepción para alerta sobre error

--- src/test/java/com/uttlaxcala/javatest/util/StringUtilTest.java
+++ src/test/java/com/uttlaxcala/javatest/util/StringUtilTest.java
@@ -4,9 +4,15 @@ package com.uttlaxcala.javatest.util;
 public class StringUtilTest {
 
     public static void main(String[] args) {
-        
-            System.out.println(StringUtil.repetir("hola", 3));
-            System.out.println(StringUtil.repetir("hola", 1));
 
+        assertEquals(StringUtil.repetir("hola", 3), "holaholahola");
+        assertEquals(StringUtil.repetir("hola", 1), "hola");
+
+    }
+
+    private static void assertEquals(String actual, String expected){
+        if (!actual.equals(expected)){
+            throw new RuntimeException(actual + " is not equal to expected " + expected);
+        }
     }
 }
