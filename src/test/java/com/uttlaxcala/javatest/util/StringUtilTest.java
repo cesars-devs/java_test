61a4ce682e21bbcfdc350d85aa04b8ae012909ce
Cesar Lima <cjli.x3@gmail.com>
2021-08-15 23:52:43 -0500

    Modicación de las clases StringUtil

---
+++ src/test/java/com/uttlaxcala/javatest/util/StringUtilTest.java
@@ -0,0 +1,12 @@
+package com.uttlaxcala.javatest.util;
+
+
+public class StringUtilTest {
+
+    public static void main(String[] args) {
+        
+            System.out.println(StringUtil.repetir("hola", 3));
+            System.out.println(StringUtil.repetir("hola", 1));
+
+    }
+}
