f4dad6b74864413e6f93f38eccfe6abe64b96129
Cesar Lima <cjli.x3@gmail.com>
2021-08-16 01:06:21 -0500

    2.4 Organización de test con JUnit

--- src/main/java/com/uttlaxcala/javatest/util/StringUtil.java
+++ src/main/java/com/uttlaxcala/javatest/util/StringUtil.java
@@ -5,6 +5,9 @@ public class StringUtil {
 
     public static String repetir(String str, int times){
         String result="";
+        if (times < 0){
+            throw new IllegalArgumentException("Negative times not allowed");
+        }
         for (int i = 0; i < times; i++) {
             result += str;
         }
--- src/test/java/com/uttlaxcala/javatest/util/StringUtilTest.java
+++ src/test/java/com/uttlaxcala/javatest/util/StringUtilTest.java
@@ -7,10 +7,29 @@ import org.junit.Test;
 public class StringUtilTest {
 
     @Test
-    public void testRepetir() {
+    public void repetir_string_once() {
+
+        Assert.assertEquals("hola", StringUtil.repetir("hola", 1) );
+
+    }
+
+    @Test
+    public void repetir_string_multiple_times() {
 
         Assert.assertEquals("holaholahola", StringUtil.repetir("hola", 3) );
-        Assert.assertEquals("hola", StringUtil.repetir("hola", 1) );
+    }
+
+    @Test
+    public void repetir_string_zero_times() {
+
+        Assert.assertEquals("", StringUtil.repetir("hola", 0) );
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void repetir_string_negative_times() {
+
+        Assert.assertEquals("", StringUtil.repetir("hola", -1) );
 
     }
 }
