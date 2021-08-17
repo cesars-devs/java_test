4e729209f879abf750b31efbf3e64689020461ff
Cesar Lima <cjli.x3@gmail.com>
2021-08-17 18:40:20 -0500

    Creación de metodo isEmpty y tests correspondientes

--- src/main/java/com/uttlaxcala/javatest/util/StringUtil.java
+++ src/main/java/com/uttlaxcala/javatest/util/StringUtil.java
@@ -3,6 +3,14 @@ package com.uttlaxcala.javatest.util;
 
 public class StringUtil {
 
+    public static boolean isEmpty(String str){
+        if (str == "" || str == " " || str == null){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
     public static String repetir(String str, int times){
         String result="";
 
--- src/test/java/com/uttlaxcala/javatest/util/StringUtilTest.java
+++ src/test/java/com/uttlaxcala/javatest/util/StringUtilTest.java
@@ -2,9 +2,40 @@ package com.uttlaxcala.javatest.util;
 
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class StringUtilTest {
+    private StringUtil string;
+
+    @Before
+    public void setup(){
+        string = new StringUtil();
+    }
+
+    @Test
+    public void is_not_empty(){
+
+        Assert.assertEquals(false, string.isEmpty("a2s"));
+    }
+
+    @Test
+    public void is_empty_when_string_is_null(){
+
+        Assert.assertEquals(true, string.isEmpty(null));
+    }
+
+    @Test
+    public void is_empty_when_string_is_space(){
+
+        Assert.assertEquals(true, string.isEmpty(" "));
+    }
+
+    @Test
+    public void is_empty(){
+
+        Assert.assertEquals(true, string.isEmpty(""));
+    }
 
     @Test
     public void repetir_string_once() {
